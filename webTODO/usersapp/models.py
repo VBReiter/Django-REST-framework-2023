54e7cbaa76ce796a6b5764d8d26f40c9472ce941
reikris <reikris@VirtValeria.myguest.virtualbox.org>
2023-03-04 20:31:50 +0300

    lesson_1

---
+++ webTODO/usersapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=64, unique=True)
+    firstname = models.CharField(max_length=64)
+    lastname = models.CharField(max_length=64)
+    email = models.CharField(max_length=40, unique=True)
+
+
